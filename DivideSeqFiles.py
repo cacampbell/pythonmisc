a87fb384b31329b528be0bc965fc8c2c0ff54bdc
cacampbell <cacampbell@ucdavis.edu>
2016-02-07 12:24:43 -0800

    Periodic Update

--- DivideSeqFiles.py
+++ DivideSeqFiles.py
@@ -3,7 +3,8 @@ import os
 import shutil
 
 
-root_directory = "/Users/sara/Accession_sequencing/Before_trimming"
+output_directory = 'qc/before'
+root_directory = 'fastq'
 prefixes = []  # Will hold file prefixes, the sample name
 directory_list = []  # Will hold directories
 
@@ -20,7 +21,7 @@ for item in os.listdir(root_directory):
 prefixes_ = prefixes[:]  # Make a copy
 for prefix in set(prefixes_):  # For each unique prefix
     try:
-        sample = os.path.join(root_directory, prefix)
+        sample = os.path.join(output_directory, prefix)
         os.makedirs(sample)  # Make the unique directories (samples)
     except Exception as e:
         print "Could not make directory: %s. Exception: %s" % (sample, str(e))
@@ -28,7 +29,7 @@ for prefix in set(prefixes_):  # For each unique prefix
 for index, directory in enumerate(directory_list):
     try:
         sample = os.path.join(root_directory, prefixes[index])
-	shutil.move(directory, sample)  # Move each directory to its respective sample
+        shutil.move(directory, sample)  # Move each directory to its sample
     except Exception as e:
         print "Could not move diectory %s to %s. Exception: %s" \
             % (directory, sample, str(e))
---
+++ Psme_qc.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import os
+import sys
+import subprocess
+import re
+from clusterlib.scheduler import submit
+from clusterlib.scheduler import queued_or_running_jobs
+from module_loader import module
+
+
+module_args = ['java']
+suffix = "fastq"
+job_prefix = "BBDuk_"
+partition = "bigmemm"
+maxmem = "200"
+maxcpu = "24"
+input_dir = "/group/nealedata4/Psme_reseq/fastq"
+output_dir = "/group/nealedata4/Psme_reseq/clean"
+adapters = "/group/nealedata4/Psme_reseq/qc/truseq.fa"
+email = "cacampbell@ucdavis.edu"
+dry_run = False
+verbose = False
+
+
+def dispatch_to_slurm(commands):
+    scripts = {}
+
+    for job_name, command in commands.iteritems():
+        script = submit(command, job_name=job_name, time="0",
+                        memory="{}G".format(maxmem), backend="slurm",
+                        shell_script="#!/usr/bin/env bash")
+        script += " --partition={}".format(partition)
+        script += " --ntasks=1"
+        script += " --cpus-per-task={}".format(maxcpu)
+        script += " --mail-type=END,FAIL"
+        script += " --mail-user={}".format(email)
+        scripts[job_name] = script
+
+    scheduled_jobs = set(queued_or_running_jobs())
+
+    for job_name, script in scripts.iteritems():
+        if job_name not in scheduled_jobs:
+            if verbose:
+                print("{}".format(script), file=sys.stdout)
+
+            if not dry_run:
+                subprocess.call(script, shell=True)
+        else:
+            print("{} already running, skipping".format(job_name),
+                  file=sys.stderr)
+
+
+def remove_ends(filelist):
+    new_filelist = []
+
+    for filename in filelist:
+        new_name = re.sub("R[0-9]", "R#", filename)
+        new_filelist += [new_name]
+
+    return set(new_filelist)
+
+
+def make_commands(filenames):
+    commands = {}
+    filenames = remove_ends(filenames)
+
+    for filename in filenames:
+        job_name = job_prefix + "_{}".format(os.path.basename(filename))
+        output_f = os.path.join(output_dir, os.path.basename(filename))
+        command = "bbduk.sh -Xmx32g -Xms30g in={filename} out={output} \
+            minlen=25 qtrim=rl trimq=10 ktrim=r k=25 mink=11 ref={adpts}\
+            hdist=1".format(filename=filename, output=output_f, adpts=adapters)
+        commands[job_name] = command
+
+        if verbose:
+            print(command, file=sys.stdout)
+
+    return commands
+
+
+def get_files(directory):
+    filelist = []
+
+    for root, directories, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename.endswith(suffix):
+                abs_path = os.path.join(root, filename)
+
+                if verbose:
+                    print(abs_path, file=sys.stdout)
+
+                filelist += [abs_path]
+
+    return filelist
+
+
+def load_modules(module_args):
+    try:
+        args = ['load']
+        args.extend(module_args)
+        module(args)
+    except Exception as e:
+        print("Could not load module: {}".format(e), file=sys.stderr)
+
+
+def main(root):
+    if verbose:
+        print("Loading Modules...", file=sys.stdout)
+
+    load_modules(module_args)
+
+    if verbose:
+        print("Gathering Files...", file=sys.stdout)
+
+    filenames = get_files(root)
+
+    if verbose:
+        print("Making Commands...", file=sys.stdout)
+
+    commands = make_commands(filenames)
+
+    if verbose:
+        print("Dispatching to Slurm...", file=sys.stdout)
+
+    dispatch_to_slurm(commands)
+
+
+if __name__ == "__main__":
+    main(input_dir)
