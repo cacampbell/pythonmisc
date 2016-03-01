95039f106fd1ecd0c584fbaea7be9c444f6d1e81
Chris Campbell <cacampbell@ucdavis.edu>
2016-03-01 13:05:27 -0800

    update

---
+++ BBMap_adapters.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import os
+import sys
+from re import sub
+from subprocess import call
+from module_loader import module
+from clusterlib.scheduler import 
