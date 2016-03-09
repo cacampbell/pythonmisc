2a424a4efccc1cb1c64804d4c289b056e13edfb1
cacampbell <cacampbell@ucdavis.edu>
2016-03-09 13:44:50 -0800

    Fixed quality control

--- quality_control.py
+++ quality_control.py
@@ -6,13 +6,17 @@ import sys
 def main(input_root, output_root, adapters):
     qc = QualityControl(input_root, output_root)
     qc.job_prefix = "QC_"
-    qc.job_prefix = ".fq.gz"
+    qc.input_suffix = ".fq.gz"
     qc.modules = ['java']
     qc.slurm_options['partition'] = 'bigmemm'
     qc.slurm_options['mail-user'] = 'cacampbell@ucdavis.edu'
     qc.slurm_options['mem'] = '80G'
     qc.slurm_options['cpus'] = '10'
     qc.reference = adapters
+    qc.read_marker = "_1"
+    qc.mate_marker = "_2"
+    qc.verbose = False
+    qc.dry_run = False
     qc.run()
 
 
