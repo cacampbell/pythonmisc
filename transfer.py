3cc9f248466b22ad834ee713ebd90226444c8cd7
Chris Campbell <cacampbell@ucdavis.edu>
2016-04-07 17:53:06 -0700

    Welp, a lot of the commands were wrong, and I had a lot of difficulty getting things to work. I guess this is why you test as you go :/. Deleted most of what I had and started the transfer part from scratch. I now have a better understanding of the commands involved.

--- transfer.py
+++ transfer.py
@@ -5,17 +5,13 @@ from fabric.api import run
 from fabric.api import settings
 from fabric.exceptions import NetworkError
 from fabric.api import hide
+from getpass import getuser
 from os import path
 import paramiko
 from re import search
 from sys import argv
 from multiprocessing import cpu_count
 from multiprocessing import Pool
-
-
-class FabricAbortException(Exception):
-    def __init__(self, message):
-        super(Exception, self).__init__(message)
 
 
 __USAGE_STATEMENT = """
@@ -43,177 +39,82 @@ __USAGE_STATEMENT = """
     [destination] is the absolute path to the destination directory
     [threads] is the number of parallel rsync threads to run
 """
+__local_help__ = """
+    It appears that the local transfer has failed. This is sexecuted by running
+    rsync -avz {src_path} {dest_path} on {server}.
 
-__retry_help__ = """
-    All methods of transfer have failed. In order, this script connects:
+    If you believe that you should be able to make this transfer, try running
+    the rsync commands manually for some subset of the files to see if you are
+    able.
+"""
 
-        (1) Directly from {src} to {dest}
-        (2) Directly from {dest} to {src}
-        (3) By tunneling from {src} --> localhost --> {dest}
-        (4) By tunneling from {dest} --> localhost --> {src}
+
+__direct_help__ = """
+    All methods of direct transfer have failed. In order, this script
+    connects:
+
+        (1) From {src} to {dest}, running rsync on {src}
+        (2) From {src} to {dest}, using reverse tunnel, running rsync on {dest}
+        (3) From {dest} to {src}, running rsync on {dest}
+        (4) From {dest} to {src}, using reverse tunnel, running rsync on {src}
 
     If these methods all failed, but you believe that you should be able to
     connect in one of these ways, it may help to attempt to connect manually
-    with ssh first. This will allow addition of hosts, unlocking of keys, and
-    will provide information requisite for debugging connections between hosts.
-    Once a connection has been successfully established with one of the above
-    methods, retry this script again.
+    first. This will provide additional diagnostic information and will
+    establish known_hosts, ssh identities, etc. Once you are able to
+    successfully connect with ssh, try this script again.
+
+        (1) {src}$ ssh -v -v -v {dest}
+        (2) {src}$ ssh -v -v -v -R {port}:localhost:22 {dest}
+            {dest}$ ssh -v -v -v -p {port} {whoami}@localhost
+        (3) {dest}$ ssh -v -v -v {src}
+        (4) {dest}$ ssh -v -v -v -R {port}:localhost:22 {src}
+            {dest}$ ssh -v -v -v -p {port} {whoami}@localhost
+
+"""
+
+
+__intermediate_help__ = """
+    All methods of intermediate transfer have failed. In order,
+    this script connects:
+
+        (1) Directly [see direct help]
+        (2) By tunneling from {src} --> localhost --> {dest},
+            running rsync on {src}
+        (3) By tunneling from {dest} --> localhost --> {src},
+            running rsync on {dest}
+
+    If these methods all failed, but you believe that you should be able to
+    connect in one of these ways, it may help to attempt to connect manually
+    first. This will provide additional diagnostic information and will
+    establish known_hosts, ssh identities, etc. Once you are able to
+    successfully connect with ssh, try this script again.
 
     Manual Commands:
-        (1) localhost$ ssh -v -v -v {src}
-            {src}$ ssh -v -v -v {dest}
-        (2) localhost$ ssh -v -v -v {dest}
-            {dest}$ ssh -v -v -v {src}
+        (2) localhost$ ssh -v -v -v -R localhost:{port}:{dest}:22 {src}
+            {src}$ ssh -v -v -v -p {port} {whoami}@localhost
         (3) localhost$ ssh -v -v -v -R localhost:{port}:{src}:22 {dest}
-            {dest}$ ssh -v -v -v -p {port} localhost
-        (4) localhost$ ssh -v -v -v -R localhost:{port}:{dest}:22 {src}
-            {src}$ ssh -v -v -v -p {port} localhost
+            {dest}$ ssh -v -v -v -p {port} {whoami}@localhost
 """
 
 serverinfo = namedtuple('serverinfo', ['host', 'path', 'rsync_base'])
 
 
-def safe_call(command):
-    try:
-        # run(command)
-        print(command)
-    except FabricAbortException as err:
-        print("Failed: {}".format(err))
-    except (NetworkError, EOFError) as err:
-        print("Failed: {}".format(err))
-        print("Remote server issued hard disconnect...")
-        raise(err)
-    finally:
-        with settings(warn_only=True):
-            m = search(":/.*/\s*?", command)
-            run(("kill -9 `ps aux | grep [rsync|parallel] | {} | "
-                 "awk '{print $2}'`").format(m.group(1)))
+class FabricAbortException(Exception):
+    def __init__(self, message):
+        super(FabricAbortException, self).__init__(message)
 
 
-def e_quotes(string):
-    return string.replace('"', '\\"').replace("'", "\\'")
+def double_remote_transfer():
+    pass
 
 
-def fab_parallel_2h(src, dest, cmds):
-    extended_commands = ("ssh -o \"NumberOfPasswordPrompts 0\" "
-                         "-R localhost:{p}:{s_h}:22 {d_h} "
-                         "\"{{cmd}}\"").format(s_h=src.host, d_h=dest.host)
-    commands = [extended_commands.format(cmd=c) for c in cmds]
-    pool = Pool(processes=env.CPUs)
-    pool.map(safe_call, commands)
+def single_remote_transfer():
+    pass
 
 
-def gnu_parallel_2h(src, dest, cmds):
-    cmds_str = '\n'.join(cmds)
-    safe_call(("ssh -o \"NumberOfPasswordPrompts 0\" "
-               "-R localhost:{p}:{s_h}:22 {d_h} \"echo -e '{c}' "
-               "| parallel --gnu -j {jobs} {{}}\"").format(p=env.bridge_port,
-                                                           s_h=src.host,
-                                                           d_h=dest.host,
-                                                           c=cmds_str,
-                                                           jobs=env.CPUs))
-
-
-def fab_parallel_1h(cmds):
-    pool = Pool(processes=env.CPUs)
-    pool.map(safe_call, cmds)
-
-
-def gnu_parallel_1h(cmds):
-    cmds_str = '\n'.join(cmds)
-    safe_call("echo -e '{}' | parallel --gnu -j {} {{}}".format(cmds_str,
-                                                                env.CPUs))
-
-
-def intermediate_commands(src, dest, files):
-    c = ("rsync -e 'ssh -o \"NumberOfPasswordPrompts 0\" -p {p}' "
-         "-avz {s} localhost:{d}")
-    return [e_quotes(c).format(s=path.join(src.path, f),
-                               d=path.join(dest.path, f),
-                               p=env.bridge_port) for f in files]
-
-
-def intermediate_transfer(src, dest, files):
-    env.host = 'localhost'
-    print("Trying {} --> localhost --> {}...".format(src.host, dest.host))
-
-    try:
-        print("Trying gnu parallel...")
-        commands = intermediate_commands(src, dest, files)
-        gnu_parallel_2h(src, dest, commands)
-    except Exception as err:
-        print("Failed: {}".format(err))
-        try:
-            print("Trying parallel fabric connections...")
-            commands = intermediate_commands(src, dest, files)
-            fab_parallel_2h(src, dest, commands)
-        except Exception as err:
-            print("Failed: {}".format(err))
-            raise(err)
-
-
-def direct_transfer(cmds, host):
-    env.host_string = host
-
-    try:
-        print("Trying gnu parallel...")
-        gnu_parallel_1h(cmds)
-    except Exception as err:
-        print("Failed: {}".format(err))
-        try:
-            print("Trying parallel fabric connections...")
-            fab_parallel_1h(cmds)
-        except Exception as err:
-            print("Failed: {}".format(err))
-            raise(err)
-
-
-def format_commands(src, dest, files):
-    c = ("rsync -e 'ssh -o \"NumberOfPasswordPrompts 0\"' "
-         "-avz {src_b}{{src_f}} {dest_b}{{dest_f}}").format(
-        src_b=src.rsync_base, dest_b=dest.rsync_base)
-    return [e_quotes(c).format(src_f=f, dest_f=f) for f in files]
-
-
-def one_remote(src, dest, files):
-    cmds = format_commands(src, dest, files)
-    direct_transfer(cmds, "localhost")
-
-
-def to_localhost(data):
-    local_data = serverinfo(host="localhost",
-                            path=data.path,
-                            rsync_base=data.path)
-    return local_data
-
-
-def two_remote(src, dest, files):
-    try:
-        print("Trying forward transfer...")
-        modified_src = to_localhost(src)
-        commands = format_commands(modified_src, dest, files)
-        direct_transfer(commands, src.host)
-    except Exception as err:
-        print("Failed: {}".format(err))
-        try:
-            print("Trying reverse transfer...")
-            modified_dest = to_localhost(dest)
-            commands = format_commands(src, modified_dest, files)
-            direct_transfer(commands, dest.host)
-        except Exception as err:
-            print("Failed: {}".format(err))
-            try:
-                print("Trying intermediate transfer...")
-                intermediate_transfer(src, dest, files)
-            except Exception as err:
-                print("Failed: {}".format(err))
-                try:
-                    intermediate_transfer(dest, src, files)
-                except Exception as err:
-                    print(__retry_help__.format(src=src.host,
-                                                dest=dest.host,
-                                                port=env.bridge_port))
-                    raise(err)
+def local_transfer():
+    pass
 
 
 def make_directories(dest, directories):
@@ -225,8 +126,8 @@ def make_directories(dest, directories):
 
 def get_files(src):
     env.host_string = src.host
-    command = ("find {}/ -type f | sed -n 's|^{}/||p'".format(src.path,
-                                                              src.path))
+    command = ("find {} -type f | sed -n 's|^{}||p'".format(src.path,
+                                                            src.path))
     background = run("").stdout.splitlines()
     background += ['\s+']
     out = run(command).stdout.splitlines()
@@ -256,40 +157,42 @@ def parse_hostname_path(hostname_path):
 
 
 def set_up(src, dest, threads):
-    env.CPUs = threads
-    env.colorize_errors = True
-    env.use_ssh_config = True
-    env.abort_on_prompts = True
-    env.abort_exception = FabricAbortException
-    env.bridge_port = '5555'
-    paramiko.util.log_to_file("/dev/null")
-    files = get_files(src)  # get files on host
-    directories = set([path.dirname(x) for x in files])
-    make_directories(dest, directories)  # make directories on destination
-    return files
+    try:
+        env.CPUs = threads
+        env.whoami = getuser()
+        env.colorize_errors = True
+        env.use_ssh_config = True
+        env.abort_on_prompts = True
+        env.abort_exception = FabricAbortException
+        env.bridge_port = '4245'  # Some large port that isn't used by anything
+        paramiko.util.log_to_file("/dev/null")  # squelch warnings about log
+        files = get_files(src)  # get files on host
+        directories = set([path.dirname(x) for x in files])
+        make_directories(dest, directories)  # make directories on destination
+        return(files)  # hand off file list
+    except (NetworkError, FabricAbortException, OSError) as err:
+        print("Error during setup: {}".format(err))
+        raise(err)
 
 
 def main(source, destination, threads=cpu_count()):
     src = parse_hostname_path(source)
     dest = parse_hostname_path(destination)
     files = set_up(src, dest, threads)
-    with settings(hide('everything')):
-        if src.host != "localhost" and dest.host != "localhost":
-            if src.host != dest.host:
-                # Neither is localhost, but have different names
-                return two_remote(src, dest, files)
 
-        # One is localhost, or transfering from remote server to itself
-        return one_remote(src, dest, files)
+    if src.host != dest.host:
+        if src.host == "localhost" or dest.host == "localhost":
+            single_remote_transfer(src, dest, files)
+        else:
+            double_remote_transfer(src, dest, files)
+    else:
+        local_transfer(src, dest, files)
 
 
 if __name__ == "__main__":
     if len(argv) == 4:
-        # Use threadcount
         main(argv[1], argv[2], argv[3])
     elif len(argv) == 3:
-        # No threadcount specified, use autodetected threads
         main(argv[1], argv[2])
     else:
-        # Usage
         print(__USAGE_STATEMENT)
