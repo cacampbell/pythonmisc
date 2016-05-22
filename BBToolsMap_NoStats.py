b53cd6d02ab97e8057d5c591bb85d14e35b16524
Chris Campbell <cacampbell@ucdavis.edu>
2016-05-22 16:41:35 -0700

    Fixed Command Formatting

--- BBToolsMap_NoStats.py
+++ BBToolsMap_NoStats.py
@@ -12,6 +12,7 @@ class BBMapperNoStats(PairedEndCommand):
 
     def make_command(self, read):
         # Mate File
+        print()
         mate = self.mate(read)
 
         # Mapped Sam file
@@ -19,16 +20,13 @@ class BBMapperNoStats(PairedEndCommand):
         map_sam = self.replace_extension(".sam", map_sam)
         map_sam = self.rebase_file(map_sam)
 
-        # Unmapped Sam file
-        unmap_sam = self.replace_extension(".unmapped.sam", read)
 
         # Full Command
-        command = ("bbmap.sh in1={i1} in2={i2} outm={om} outu={ou} nodisk "
-                   "threads={t} ref={r} slow k=12 -Xmx{xmx} basecov={basecov} "
+        command = ("bbmap.sh in1={i1} in2={i2} outm={om} nodisk "
+                   "threads={t} ref={r} slow k=12 -Xmx{xmx} "
                    "usejni=t").format(i1=read,
                                       i2=mate,
                                       om=map_sam,
-                                      ou=unmap_sam,
                                       xmx=self.get_mem(),
                                       t=self.get_threads(),
                                       r=self.reference)
