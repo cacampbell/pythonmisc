1a98e33e159df6147dd1380e9f7e5ac02cc8eca9
Chris Campbell <cacampbell@ucdavis.edu>
2016-05-26 12:52:21 -0700

    Adding Mapping function

--- bbtools_map.py
+++ bbtools_map.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python
+from BBToolsMap import BBMapper
+from BBToolsMap_NoStats import BBMapperNoStats
+from simple_argparse import run_parallel_command_with_args
+
+
+def main(*args, **kwargs):
+    mapper = BBMapperNoStats(args, kwargs)
+
+    if 'stats' in kwargs:
+        if kwargs['stats'] is True:
+            mapper = BBMapper(args, kwargs)
+
+    return (mapper.run())  # List of slurm jobs generated
+
+
+if __name__ == "__main__":
+    jobs = run_parallel_command_with_args(main)
+    print(jobs)
