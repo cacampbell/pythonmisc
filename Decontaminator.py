fc287ad99953b09c296fbcd3663a15691286dd53
Chris Campbell <cacampbell@ucdavis.edu>
2016-06-13 15:25:17 -0700

    Re-added general decontamination

---
+++ Decontaminator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from PairedEndCommand import PairedEndCommand
+
+
+class Decontaminator(PairedEndCommand):
+    def __init__(self, *args, **kwargs):
+        super(Decontaminator, self).__init__(self, *args, **kwargs)
+        self.set_default("contaminants",
+                         "/home/cacampbe/.prog/bbmap/resources/"
+                         "phix_adapters.fa.gz")
+
+    def make_command(self, read):
+        mate = self.mate(read)
+        out1 = self.rebase_file(read)
+        out2 = self.rebase_file(mate)
+        outm1 = self.replace_extension_with(".contam.fq.gz", read)
+        outm1 = self.rebase_file(outm1)
+        outm2 = self.replace_extension_with(".contam.fq.gz", mate)
+        outm2 = self.rebase_file(outm2)
+        stats = self.replace_read_marker_with("_stats", read)
+        stats = self.replace_extension_with(".txt", stats)
+        stats = self.rebase_file(stats)
+        command = ("bbduk.sh -Xmx{xmx} threads={t} in1={i1} in2={i2} out1={o1} "
+                   "out2={o2} outm1={m1} outm2={m2} stats={s} ref={r} k=31 "
+                   "hdist=1").format(
+            xmx=self.get_mem(fraction=0.95), t=self.get_threads(), in1=read,
+            in2=mate, o1=out1, o2=out2, m1=outm1, m2=outm2, r=self.reference,
+            s=stats
+        )
+        return (command)
--- decontaminate.py
+++ decontaminate.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-from Decontaminate import Decontaminate
-
+from Decontaminator import Decontaminator
 from simple_argparse import run_parallel_command_with_args
 
 
 def main(*args, **kwargs):
-    decontaminator = Decontaminate(*args, **kwargs)
+    decontaminator = Decontaminator(*args, **kwargs)
     return(decontaminator.run())
 
 
