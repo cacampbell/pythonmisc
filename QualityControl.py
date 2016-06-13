41d0ddae32df85af3b9dd480bc5f0cf191b91f3d
Chris Campbell <cacampbell@ucdavis.edu>
2016-06-13 16:14:12 -0700

    Added different passes to the QualityControl script

--- QualityControl.py
+++ QualityControl.py
@@ -12,9 +12,44 @@ class QualityControl(PairedEndCommand):
         mate = self.mate(read)
         output1 = self.rebase_file(read)
         output2 = self.rebase_file(mate)
-        command = ("bbduk2.sh -Xmx{maxheap} threads={t} in1={i1} in2={i2} "
-                   "out1={o1} out2={o2} ref={adpt} ktrim=r ftm=5 "
-                   "k=25 mink=11 hdist=2 tpe tbo qtrim=rl trimq=1").format(
-            maxheap=self.get_mem(fraction=0.95), i1=read, i2=mate, o1=output1,
-            o2=output2, adpt=self.reference, t=self.get_threads())
+        stats1 = self.replace_read_marker_with("_stats1", read)
+        stats1 = self.replace_extension_with(".txt", stats1)
+        stats1 = self.rebase_file(stats1)
+        stats2 = self.replace_read_marker_with("_stats2", read)
+        stats2 = self.replace_extension_with(".txt", stats2)
+        stats2 = self.rebase_file(stats2)
+        # First Run
+        # force trim module 5 -- gets rid of extra bases
+        # trim matching adapters with k = 27 and min k = 11 on ends
+        # allow up to two mismatches
+        # force trim pairs to same length
+        # trim by pair overlap detection
+        # trim adapters from the right (3' adapters)
+        #
+        # Second Run
+        # kmer-trim size 23 or above matches to the reference, with end
+        # trimming of matches of 11 base pairs or more, with up to zero
+        # mismatches with the reference.
+        # Force trim both sets of a pair to the same length (tpe)
+        # Trim adapters by pair overlap detection (tbo)
+        # Trim adapters from the right (3' adapters)
+        # trim quality from both ends, using quality score 5 to filter
+        # TODO: Pipe paired files from first pass to second pass with stdout
+        # TODO: test piping from stdout
+        command = ("bbduk.sh -Xmx{xmx} threads={t} "
+                   "in1={i1} in2={i2} out1=stdout1.fq out2=stdout2.fq "
+                   "stats={s1} ref={a} ktrim=r k=27 mink=11 hdist=2 tpe tbo | "
+                   "bbduk.sh -Xmx{xmx} threads={t} "
+                   "in1=stdin1.fq in2=stdin2.fq out1={o1} out2={o2} stats={s2} "
+                   "ref={a} ktrim=r k=23 mink=11 hdist=0 tpe tbo "
+                   "qtrim=rl trimq=5").format(
+            xmx=self.get_mem(fraction=0.95),
+            t=self.get_threads(),
+            i1=read,
+            i2=mate,
+            o1=output1,
+            o2=output2,
+            s1=stats1,
+            s2=stats2,
+            a=self.reference)
         return command
--- decontaminate.py
+++ decontaminate.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 from Decontaminator import Decontaminator
+
 from simple_argparse import run_parallel_command_with_args
 
 
 def main(*args, **kwargs):
     decontaminator = Decontaminator(*args, **kwargs)
-    return(decontaminator.run())
+    return (decontaminator.run())
 
 
 if __name__ == "__main__":
