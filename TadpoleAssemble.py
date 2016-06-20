565be57bfac41b0be91ceaa3eaa79fab2cfe07ac
Chris Campbell <cacampbell@ucdavis.edu>
2016-06-20 16:55:14 -0700

    Added Assembly. Thinking about the weird use of set_default.

--- TadpoleAssemble.py
+++ TadpoleAssemble.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python3
+from sys import stderr
+
+from os.path import basename
+
 from PairedEndCommand import PairedEndCommand
+from combiner import combine_files
 
 
 class TadpoleAssemble(PairedEndCommand):
     def __init__(self, *args, **kwargs):
         super(TadpoleAssemble, self).__init__(*args, **kwargs)
+        self.set_default("all_reads_name", "all_reads.fq.gz")
+        self.set_default("assembly_name", "contigs.fa")
+        self.set_default("mincontig", "250")
+        self.set_default("mincov", "3")
 
-    def make_command(self, files):
+    def make_command(self, read):
         pass
+
+    def format_commands(self):
+        combine_files(self.files, self.all_reads_name)
+        job_name = "{}{}".format(self.cluster_options["job_name"],
+                                 basename(self.all_reads_name))
+        command = ("tadpole.sh in={} out={} careful prealloc=t prefilter=2 "
+                   "mincontig={} mincov={}").format(self.all_reads_name,
+                                                    self.assembly_name,
+                                                    self.mincontig,
+                                                    self.mincov)
+        self.commands[job_name] = command
+
+        if self.verbose:
+            print(command, file=stderr)
