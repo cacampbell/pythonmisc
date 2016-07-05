d020e82963854335de8d7a96efb8b04d49abc119
Chris Campbell <cacampbell@ucdavis.edu>
2016-07-05 11:09:34 -0700

    Added RNA-Seq specific parameters to mapping

--- BBToolsMap_NoStats.py
+++ BBToolsMap_NoStats.py
@@ -6,9 +6,10 @@ class BBMapperNoStats(PairedEndCommand):
     def __init__(self, *args, **kwargs):
         super(BBMapperNoStats, self).__init__(*args, **kwargs)
         self.set_default("reference", "reference.fa")
+        self.set_default("mode", "DNA")
         # Set read_regex here if necessary
 
-    def make_command(self, read):
+    def __dna_command(self, read):
         mate = self.mate(read)
         map_sam = self.replace_read_marker_with("_pe", read)
         map_sam = self.replace_extension_with(".sam", map_sam)
@@ -26,3 +27,29 @@ class BBMapperNoStats(PairedEndCommand):
                                       t=self.get_threads(),
                                       r=self.reference)
         return (command)
+
+    def __rna_command(self, read):
+        mate = self.mate(read)
+        map_sam = self.replace_read_marker_with("_pe", read)
+        map_sam = self.replace_extension_with(".sam", map_sam)
+        map_sam = self.rebase_file(map_sam)
+        unmap_sam = self.replace_read_marker_with("_pe", read)
+        unmap_sam = self.replace_extension_with(".unmapped.sam", unmap_sam)
+        unmap_sam = self.rebase_file(unmap_sam)
+        command = ("bbmap.sh in1={i1} in2={i2} outm={om} outu={ou} nodisk "
+                   "threads={t} ref={r} slow k=12 -Xmx{xmx} maxindel=100000 "
+                   "xstag=firststrand intronlen=10 ambig=random "
+                   "usejni=t").format(i1=read,
+                                      i2=mate,
+                                      om=map_sam,
+                                      ou=unmap_sam,
+                                      xmx=self.get_mem(),
+                                      t=self.get_threads(),
+                                      r=self.reference)
+        return (command)
+
+    def make_command(self, read):
+        if self.mode.upper().strip() == "DNA":
+            return (self.__dna_command(read))
+        elif self.mode.upper().strip == "RNA":
+            return (self.__rna_command(read))
