4bb36b41fb22d8b3a0d1f6fe732cde26142baf33
Chris Campbell <cacampbell@ucdavis.edu>
2016-07-06 16:01:48 -0700

    Integrated Trinity Assembly into scripts

---
+++ assemble.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from TadpoleAssemble import TadpoleAssemble
+from TrinityAssemble import TrinityAssemble
+from parallel_command_parse import run_parallel_command_with_args
+
+
+def main(*args, **kwargs):
+    if "assembler" in kwargs.keys():
+        if kwargs["assembler"].upper().strip() == "TADPOLE":
+            assembler = TadpoleAssemble(*args, **kwargs)
+            assembler.input_regex = ".*"
+            assembler.read_regex = ".*"
+            # assembler.extension = r".fq.gz"
+            assembler.modules = ['java/1.8', 'slurm']
+            jobs = assembler.run()
+            return (jobs)
+        elif kwargs["assembler"].upper().strip() == "TRINITY":
+            assembler = TrinityAssemble(*args, **kwargs)
+            assembler.input_regex = ".*"
+            assembler.read_regex = ".*"
+            if "genome_guided" in kwargs.keys():
+                assembler.extension = ".bam"
+                assembler.modules = ["java/1.8", "samtools/1.3.1", "slurm"]
+                jobs = assembler.run()
+                return (jobs)
+            else:
+                assembler.extension = ".fq"
+                assembler.modules = ["java/1.8", "slurm"]
+                jobs = assembler.run()
+                return (jobs)
+
+
+if __name__ == "__main__":
+    jobs = run_parallel_command_with_args(main)
+    print(jobs)
