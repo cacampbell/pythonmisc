1e7a9045f482bd8958a7d23fa6747403ad850d2a
cacampbell <cacampbell@ucdavis.edu>
2016-08-08 09:45:36 -0700

    Updated mapping tests

--- map_tests.py
+++ map_tests.py
@@ -1,52 +1,65 @@
 #!/usr/bin/env python3
 from Bash import bash
+from shlex import split
+from sys import stderr
 
 
 def main():
     speeds = ["vfast", "fast", "normal", "slow", "vslow"]
-    modulos = ["--usemodulo", ""]
+    modulos = ["--usemodulo"]
     stats = ["--stats", ""]
-    reads = ["100", "1000", "10000"]
+    reads = ["100", "10"]
 
     command = ("map.py --verbose --partition=bigmemm --memory={mem} --cpus=14 "
-               "--email_address=cacampbell@ucdavis.edu "
-               "--email_options=BEGIN,FAIL,END --input_root=ErrCorrect "
-               "--output_root={outroot} --job_name={jobname} {modulo} "
-               "--speed={speed} {stats} --read_groups --num_reads={reads}")
+               "--email_address=cacampbell@ucdavis.edu --extension=.fastq.gz$ "
+               "--email_options=BEGIN,FAIL,END --input_root=ErrCorrect.1 "
+               "--output_root={outroot} --job_name={jobname} {modulo} --pigz "
+               "--speed={speed} {stats} --read_groups --num_reads={reads} "
+               "ref=pila.v1.0.scafSeq.fa nodisk")
 
-    for speed in speeds:
-        for modulo in modulos:
-            for stat in stats:
-                for read in reads:
-                    mem = ""
-                    if "--usemodulo" == modulo:
+    for i, speed in enumerate(speeds):
+        for j, modulo in enumerate(modulos):
+            for k, stat in enumerate(stats):
+                for l, read in enumerate(reads):
+                    mem = "300G"
+                    if j == 0:
                         mem = "200G"
-                    else:
-                        mem = "300G"
 
-                    jobname = "Pwal_Map_Test_{}.{}.{}.{}".format(
-                        speed,
-                        modulo.rstrip("--"),
-                        stat.rstrip("--"),
-                        read
+                    def __opts():
+                        s = speed
+                        m = "no_modulo"
+                        st = "no_stats"
+                        r = read
+
+                        if j == 0:
+                            m = "modulo"
+
+                        if k == 0:
+                            st = "stats"
+
+                        return {'s':s, 'm':m, 'st':st, 'r':r}
+
+                    jobname = "Map_Test.{s}.{m}.{st}.{r}.".format(
+                        **__opts()
                     )
 
-                    outroot = "Mapped_Test_{}.{}.{}.{}".format(
-                        speed,
-                        modulo.rstrip("--"),
-                        stat.rstrip("--"),
-                        read
+                    outroot = "Map_Tests/Map_Test.{s}.{m}.{st}.{r}".format(
+                        **__opts()
                     )
 
-                    bash(command.format(
+                    args = split(command.format(
                         speed=speed,
                         modulo=modulo,
                         stats=stat,
-                        reads=reads,
+                        reads=read,
                         mem=mem,
                         jobname=jobname,
                         outroot=outroot
                     ))
+
+                    (out, err) = bash(*args)
+                    print(out)
+                    print(err, file=stderr)
 
 if __name__ == "__main__":
     main()
